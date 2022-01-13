23b567f11b73733f41824e3cde4501fed1b95bad
Andrew Dant <andrewdant17@gmail.com>
2022-01-13 16:17:18 -0500

    start using strings for all dictionary keys
    when it's backed up all number keys are converted to strings anyway

--- screen-spots.py
+++ screen-spots.py
@@ -9,18 +9,20 @@ spot_dictionary = storage.get("screen-spots", {})
 class SpotClass:
     def save_spot(spot_number: int):
         """Saves the current mouse position (to a specific number)"""
+        number_string = str(spot_number)
         x = actions.mouse_x()
         y = actions.mouse_y()
 
-        spot_dictionary[spot_number] = [x, y]
+        spot_dictionary[number_string] = [x, y]
 
     def move_spot(spot_number: int) -> bool:
         """
         Moves the cursor to a location, if one was saved for the given index.
         Returns true if the cursor was moved
         """
-        if spot_number in spot_dictionary:
-            spot = spot_dictionary[spot_number]
+        number_string = str(spot_number)
+        if number_string in spot_dictionary:
+            spot = spot_dictionary[number_string]
 
             actions.mouse_move(spot[0], spot[1])
             return True
@@ -39,10 +41,13 @@ class SpotClass:
 
     def drag_spot(spot_number: int):
         """Drag the mouse from its current location to the saved position (if it exists)"""
-        if spot_number in spot_dictionary:
+        number_string = str(spot_number)
+        if number_string in spot_dictionary:
             actions.user.mouse_drag(0)
-            actions.user.move_spot(spot_number)
+            actions.user.move_spot(number_string)
 
     def backup_spot():
         """Save the spot dictionary to be used again upon reload"""
         storage.set("screen-spots", spot_dictionary)
+
+
