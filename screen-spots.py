4c13e994bb1e7abc723b2672d9b885a5ffe17f7b
Andrew Dant <andrewdant17@gmail.com>
2022-01-14 11:49:33 -0500

    spot keys no longer restricted to numbers

--- screen-spots.py
+++ screen-spots.py
@@ -7,44 +7,41 @@ spot_dictionary = storage.get("screen-spots", {})
 
 @mod.action_class
 class SpotClass:
-    def save_spot(spot_number: int):
+    def save_spot(spot_key: str):
         """Saves the current mouse position (to a specific number)"""
-        number_string = str(spot_number)
         x = actions.mouse_x()
         y = actions.mouse_y()
 
-        spot_dictionary[number_string] = [x, y]
+        spot_dictionary[spot_key] = [x, y]
 
-    def move_spot(spot_number: int) -> bool:
+    def move_spot(spot_key: str) -> bool:
         """
         Moves the cursor to a location, if one was saved for the given index.
         Returns true if the cursor was moved
         """
-        number_string = str(spot_number)
-        if number_string in spot_dictionary:
-            spot = spot_dictionary[number_string]
+        if spot_key in spot_dictionary:
+            spot = spot_dictionary[spot_key]
 
             actions.mouse_move(spot[0], spot[1])
             return True
         return False
 
-    def click_spot(spot_number: int):
+    def click_spot(spot_key: str):
         """Clicks the saved mouse position (if it exists) then returns your cursor to its current position"""
         current_x = actions.mouse_x()
         current_y = actions.mouse_y()
 
-        was_moved = actions.user.move_spot(spot_number)
+        was_moved = actions.user.move_spot(spot_key)
 
         if was_moved:
             ctrl.mouse_click(button=0)
             actions.mouse_move(current_x, current_y)
 
-    def drag_spot(spot_number: int):
+    def drag_spot(spot_key: str):
         """Drag the mouse from its current location to the saved position (if it exists)"""
-        number_string = str(spot_number)
-        if number_string in spot_dictionary:
+        if spot_key in spot_dictionary:
             actions.user.mouse_drag(0)
-            actions.user.move_spot(number_string)
+            actions.user.move_spot(spot_key)
 
     def backup_spot():
         """Save the spot dictionary to be used again upon reload"""
--- screen-spots.talon
+++ screen-spots.talon
@@ -1,16 +1,16 @@
 mode: command
 -
 # save a mouse position to a spot index
-spot save <number_small>: user.save_spot(number_small)
+spot save <user.text>: user.save_spot(user.text)
 
 # click a saved spot then return the cursor
-spot (click|touch) <number_small>: user.click_spot(number_small)
+spot (click|touch) <user.text>: user.click_spot(user.text)
 
 # move the cursor to a saved spot
-spot [move] <number_small>: user.move_spot(number_small)
+spot [move] <user.text>: user.move_spot(user.text)
 
 # hold left click then move the cursor to a saved spot
-spot drag <number_small>: user.drag_spot(number_small)
+spot drag <user.text>: user.drag_spot(user.text)
 
 # save all current spots to a cache that survives reloads
 spot backup: user.backup_spot()
